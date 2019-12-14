e79611a9e5ab30cb1e32daf34669cd72bd6e02fd
Vyacheslav <dagron777@mail.ru>
2019-12-14 12:10:13 +0300

    created controllers for route group

--- server/database/migrations/2019_10_13_085345_create_routes_route_groups_table.php
+++ server/database/migrations/2019_10_13_085345_create_routes_route_groups_table.php
@@ -4,23 +4,23 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateRoutesRouteGroupTable extends Migration
+class CreateRoutesRouteGroupsTable extends Migration
 {
     public function up()
     {
-        Schema::create('routes_route_group', function (Blueprint $table) {
+        Schema::create('routes_route_groups', function (Blueprint $table) {
             $table->increments('id');
             $table->unsignedInteger('route_group_id');
             $table->unsignedInteger('route_id');
             $table->timestamps();
 
-            $table->foreign('route_group_id')->references('id')->on('route_group')->onDelete('cascade');
+            $table->foreign('route_group_id')->references('id')->on('route_groups')->onDelete('cascade');
             $table->foreign('route_id')->references('id')->on('routes')->onDelete('cascade');
         });
     }
 
     public function down()
     {
-        Schema::dropIfExists('routes_route_group');
+        Schema::dropIfExists('routes_route_groups');
     }
 }

a8a873c36af34ccd0d905f0756a3e304e83f4b8f
Vyacheslav <dagron777@mail.ru>
2020-01-12 14:39:25 +0300

    update тфьу

--- server/database/factories/UserFactory.php
+++ server/database/factories/UserFactory.php
@@ -8,8 +8,8 @@ use Illuminate\Support\Str;
 
 $factory->define(User::class, function (Faker $faker) {
     return [
-        'name' => 'Maria',
-        'last_name' => 'Ur',
+        'name' => 'Marina',
+        'last_name' => 'Yurievna',
         'email' => 'your_mail@mail.ru',
         'password' => Hash::make(
             'secret'
